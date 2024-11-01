e92f800cffe392546c6167df66cc3a8c210a9632
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-11-01 23:26:19 +0000

    save

--- src/models.py
+++ src/models.py
@@ -7,27 +7,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    contacts = relationship('Follower', back_populates="user")
+    favorites = relationship('Favorites', back_populates="user")
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Liked_Posts(Base):
+    __tablename__ = 'liked posts'
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
 
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+    
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
